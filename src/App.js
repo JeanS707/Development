7f012efe730e7c05bd1edd8e8c08209ab9af9de4
JeanS707 <74681689+JeanS707@users.noreply.github.com>
2022-05-04 18:20:54 -0400

    card moment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import "./styles.css";
 
 export default function App() {
+  // todo: create card array
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <div>{/* todo: map over cards array and return card component */}</div>
   );
 }
-// requirements:
-//- 8 cards (4 in two rows) with fronts and backs
-//- have the code read when a card has been matched
-//- if card matched, stay flipped, if not then flip back
-// if all cards matched = game end => you win
-// a randomizer of where each card goes after each reset
-// EXTRA:
-//sound effects
-// timer
-// scoreboard => move counter
 
-<div class="row1"></div>;
+// todo: make a card component
+function card: [1, 2, 3, 4];
+const cards = [
+  <card>
+];
+
+{/* <div class="row1"></div>; */}
+
+
---
+++ src/instuctions.js
@@ -0,0 +1,16 @@
+## requirements:
+
+- 8 cards (4 in two rows) with fronts and backs
+- have the code read when a card has been matched
+- if card matched, stay flipped, if not then flip back
+- if all cards matched = game end => you win
+- a randomizer of where each card goes after each reset
+
+## EXTRA:
+
+- sound effects
+- timer
+- scoreboard => move counter
+- setup- cards in array by number
+
+## STEPS ON WHAT I WANT TO DO
