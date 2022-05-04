05230112c857e50ab2618b7e08b0a27ad583bcae
JeanS707 <74681689+JeanS707@users.noreply.github.com>
2022-05-04 17:32:12 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+// requirements:
+//- 8 cards (4 in two rows) with fronts and backs
+//- have the code read when a card has been matched
+//- if card matched, stay flipped, if not then flip back
+// if all cards matched = game end => you win
+// a randomizer of where each card goes after each reset
+// EXTRA:
+//sound effects
+// timer
+// scoreboard => move counter
+
+<div class="row1"></div>;
